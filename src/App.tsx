5d383c95d96ea1f728b67acd9981516a19c3b849
Nhung <hongnhung16052003@gmail.com>
2024-11-17 12:11:45 +0700

    done case 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,112 @@
-import Circle from "./Component/Circle"
+import { useCallback, useEffect, useState } from "react";
+import Circle from "./Component/Circle";
 
 function App() {
+  const [title, setTitle] = useState<string>("LET'S PLAY");
+  const [number, setNumber] = useState<number>(0);
+  const [timeUp, setTimeUp] = useState({ seconds: 0, milliseconds: 0 });
+  const [status, setStatus] = useState<string>("ON");
+  const [starting, setStarting] = useState(false);
+  const [isPlay, setIsPlay] = useState(false);
+
+  const handleStarting = useCallback(() => {
+    setStarting(true);
+  }, []);
+
+  const handleEnd = useCallback(() => {
+    setStarting(false);
+    setTitle("ALL CLEARED");
+    setIsPlay(false)
+  }, []);
+
+  useEffect(() => {
+    if (!starting) return;
+
+    const timerId = setInterval(() => {
+      setTimeUp((prevTime) => {
+        const newMiliSeconds = prevTime.milliseconds + 1;
+        const newSeconds =
+          newMiliSeconds === 10 ? prevTime.seconds + 1 : prevTime.seconds;
+        return {
+          seconds: newSeconds,
+          milliseconds: newMiliSeconds === 10 ? 0 : newMiliSeconds,
+        };
+      });
+    }, 100);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [starting]);
+
   return (
-    <div className="text-red-400">
-      hihi
-      <Circle/>
+    <div className="flex justify-center">
+      <div className="p-7">
+        <h1
+          className="text-[20px] font-bold mb-3"
+          style={{ color: title === "ALL CLEARED" ? "green" : "black" }}
+        >
+          {title}
+        </h1>
+        <div className="flex gap-x-4 items-center mb-3">
+          <label>Points: </label>
+          <input
+            type="number"
+            min={0}
+            value={number === undefined ? "" : number}
+            onChange={(e) => setNumber(Number(e.target.value))}
+            className="w-[200px] p-1 px-2 border-[#ccc] border-[1px] rounded-[4px]"
+          />
+        </div>
+        <div className="flex gap-x-4 mb-5">
+          <label>Time:</label>
+          <span>{timeUp.seconds + ":" + timeUp.milliseconds + "s"}</span>
+        </div>
+        <div className="flex gap-x-4">
+          {title !== "LET'S PLAY" && (
+            <button className="p-2 px-4 border-[green] border-[1px] rounded-[8px] text-[green]">
+              Restart
+            </button>
+          )}
+          {!isPlay && title !== 'ALL CLEARED' && (
+            <button
+              onClick={() => setIsPlay(true)}
+              className="p-2 px-4 border-[green] border-[1px] rounded-[8px] text-[green]"
+              disabled={number === 0}
+              style={number === 0 ? { opacity: 0.5 } : {}}
+            >
+              Play
+            </button>
+          )}
+          {isPlay && (
+            <button className="p-2 px-4 border-[green] border-[1px] rounded-[8px] text-[green]">
+              Restart
+            </button>
+          )}
+          {isPlay && (
+            <button className="p-2 px-4 border-[green] border-[1px] rounded-[8px] text-[green]">
+              Auto play <span>{status}</span>
+            </button>
+          )}
+        </div>
+        <div
+          className="mt-5 w-[900px] h-[500px] bg-[#fafaed] relative"
+          style={{ border: "1px solid #ccc" }}
+        >
+          {isPlay &&
+            Array.from({ length: number }).map((_, index) => (
+              <Circle
+                key={index + 1}
+                value={index + 1}
+                handleStarting={handleStarting}
+                handleEnd={handleEnd}
+                max={number}
+              />
+            ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Component/Circle.tsx
+++ src/Component/Circle.tsx
@@ -1,10 +1,97 @@
-function Circle() {
-    return (<div className="text-red-500">
-        Hello nhé
-        <p className="mt-6">
-            Buồn quá đi
-        </p>
-    </div> );
+import { memo, useEffect, useRef, useState } from "react";
+
+function Circle({
+  value,
+  handleStarting,
+  max,
+  handleEnd,
+}: {
+  value: number;
+  handleStarting: Function;
+  handleEnd: Function;
+  max: number;
+}) {
+  const circleRef = useRef<HTMLDivElement | null>(null);
+  const [starting, setStarting] = useState(false);
+  const [time, setTime] = useState({ seconds: 1, miliSeconds: 0 });
+  const [opacity, setOpacity] = useState(1);
+  const [position, setPosition] = useState({ top: 0, left: 0 });
+  const [isFinished, setIsFinished] = useState(false);
+
+  console.log(opacity);
+
+  useEffect(() => {
+    if (isFinished) {
+      if (value === max) {
+        handleEnd();
+      }
+      setStarting(false);
+    }
+  }, [isFinished]);
+
+  const getRandomPosition = () => Math.floor(Math.random() * 90);
+
+  const handleClick = () => {
+    if (circleRef.current) {
+      circleRef.current.style.backgroundColor = "red";
+    }
+    if (value === 1) {
+      handleStarting();
+    }
+    setStarting(true);
+  };
+
+  useEffect(() => {
+    setPosition({
+      top: getRandomPosition(),
+      left: getRandomPosition(),
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!starting) return;
+    const timerId = setInterval(() => {
+      setOpacity((prev) => prev - 1.0 / 10);
+      setTime((prevTime) => {
+        if (prevTime.miliSeconds === 1 && prevTime.seconds === 0) {
+          clearInterval(timerId);
+          setIsFinished(true);
+        }
+        let newMiliSeconds = prevTime.miliSeconds - 1;
+        const newSeconds =
+          newMiliSeconds === -1 ? prevTime.seconds - 1 : prevTime.seconds;
+        return {
+          seconds: newSeconds,
+          miliSeconds: newMiliSeconds === -1 ? 9 : newMiliSeconds,
+        };
+      });
+    }, 100);
+
+    return () => clearInterval(timerId);
+  }, [starting]);
+
+  return (
+    <div
+      className="absolute rounded-full w-[50px] h-[50px] flex flex-col items-center justify-center"
+      onClick={handleClick}
+      ref={circleRef}
+      style={{
+        top: `${position.top}%`,
+        left: `${position.left}%`,
+        border: "1px solid red",
+        opacity: opacity,
+        pointerEvents: opacity < 0.05 ? "none" : "auto",
+      }}
+    >
+      {value}
+      {starting && (
+        <span className="text-[#fff] mt-1">
+          {time.seconds}:
+          {time.miliSeconds < 10 ? `0${time.miliSeconds}` : time.miliSeconds}s
+        </span>
+      )}
+    </div>
+  );
 }
 
-export default Circle;
+export default memo(Circle);
